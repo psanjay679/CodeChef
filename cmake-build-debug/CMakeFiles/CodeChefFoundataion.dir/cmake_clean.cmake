b17119f116425e0d1489e52511aaf0a1b8fef61d
Sanjay Panchal <psanjay679@gmail.com>
2019-03-20 14:29:43 +0000

    all files to git

---
+++ cmake-build-debug/CMakeFiles/CodeChefFoundataion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeChefFoundataion.dir/main.cpp.o"
+  "CodeChefFoundataion.pdb"
+  "CodeChefFoundataion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeChefFoundataion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
