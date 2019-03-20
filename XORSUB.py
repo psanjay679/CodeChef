ff59dd0280174b5e4c654aee8083b0840bdcc06d
sanjay.panchal <sanjay.panchal@fireeye.com>
2019-03-20 23:54:39 +0530

    code

--- XORSUB.py
+++ XORSUB.py
@@ -1,4 +1,15 @@
 t = int(input())
 
 for _ in range(t):
-	pass
+	n, k = map(int, input().split())
+	ar = list(map(int, input().split()))
+	maxXor = 0 ^ k
+
+	for i in range(n):
+		xor = ar[i]
+		for j in range(i + 1, n):
+			xor = ar[j]
+			maxXor = max(maxXor, k ^ xor)
+		maxXor = max(maxXor, k ^ xor)
+
+	print (maxXor)
